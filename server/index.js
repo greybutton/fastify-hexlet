2b61bf0f783fb717b10a4d128d3ae0efc9e89770
greybutton <greybutton@yandex.com>
2020-05-15 19:34:16 +0400

    add rollbar

--- server/index.js
+++ server/index.js
@@ -14,6 +14,7 @@ import fastifyReverseRoutes from 'fastify-reverse-routes';
 import fastifyMethodOverride from 'fastify-method-override';
 import Pug from 'pug';
 import i18next from 'i18next';
+import Rollbar from 'rollbar';
 import ru from './locales/ru.js';
 import webpackConfig from '../webpack.config.js';
 
@@ -26,6 +27,12 @@ import Guest from './entity/Guest.js';
 
 const isProduction = process.env.NODE_ENV === 'production';
 const isDevelopment = !isProduction;
+
+const rollbar = new Rollbar({
+  accessToken: process.env.ROLLBAR_TOKEN,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+});
 
 const setUpViews = (app) => {
   const { devServer } = webpackConfig;
@@ -117,6 +124,12 @@ export default () => {
       base: null,
     },
   });
+
+  if (isProduction) {
+    app.setErrorHandler((error, request) => {
+      rollbar.error(error, request);
+    });
+  }
 
   registerPlugins(app);
 
